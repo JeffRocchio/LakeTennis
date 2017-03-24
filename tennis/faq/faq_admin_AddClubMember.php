435b34e2b7a381d50d08fb04143eb821d73a52a2
Jeffrey Rocchio <jeffrocchio@gmail.com>
2017-03-24 13:07:49 -0400

    Add files via upload

---
+++ tennis/faq/faq_admin_AddClubMember.php
@@ -0,0 +1,124 @@
+<html><head>
+
+<LINK REL=StyleSheet HREF="../../main.css" TYPE="text/css">
+<LINK REL=StyleSheet HREF="../../main.css" TYPE="text/css" MEDIA=screen>
+
+<title>Tennis FAQ - How Do I Add a New Person to My Club?</title>
+
+</head>
+
+<body CLASS=bdyNormal>
+<p CLASS=pgHdrPurpose>FAQ</P>
+<p CLASS=pgHdrTopic1>Administrative</P>
+<p CLASS=pgHdrTopic2>How Do I Add a New Person to My Club?</P>
+<P STYLE='margin-top: 0; margin-bottom: 0'>&nbsp;</P>
+
+<P>
+
+<OL CLASS=NumL1>
+	<LI CLASS=Normal>
+		Login to your group.
+	</LI>
+		
+	<LI CLASS=Normal>
+		Click on 'Club Phone List.'
+	</LI>
+
+	<LI CLASS=Normal>
+		At the bottom of the phone list page click on 'Add Club Member'
+		under Administrative Functions.
+	</LI>
+
+	<LI CLASS=Normal>
+		Add the person:
+	</LI>
+
+	<OL CLASS=NumL2>
+	
+		<LI CLASS=Normal>
+			In the list in the right-hand column, look to see if the person
+			you want to add is in that list. (This means they already have an
+			account on the site somewhere and therefore you don't need to create a new
+			record for them.) IF they are on that list, click the 'ADD' link next to
+			their name. This will add them to your 'club.'
+		</LI>
+
+		<LI CLASS=Normal>
+			If they are not on the list in the right-hand column, then at
+			the bottom of the page click on the link 'ADD New Person' under
+			Administrative Functions. This will give you a page to enter in their
+			info. First Name, Last Name, etc. I recommend you use a standard convention
+			for their ID and Password. For example, you might decide to use 
+			ID=first-name and first initial of last name (e.g., Jeffrey Rocchio
+			would be jeffreyr) and Password=clubID # and last name (e.g., 06rocchio).
+			Two things to note here. First, this is a light-weight security system,
+			so folks should not use passwords they use for their bank and such.
+			Second, the ID must be unique across the whole site; so for example you
+			can't create two people whose ID's are 'smith.' If you attempt to do so
+			you you'll get an ugly error message when you go to save the page (I 
+			am not yet catching this error to give a nice user-friendly page so all
+			you get is a cryptic error from the database which is trying to tell
+			you that it can't add the new person due to the duplicate ID.)
+		</LI>
+	
+		<LI CLASS=Normal>
+			Once you have done either step 4.a or 4.b above the new member
+			will appear in the left-hand column of the "Add and Remove People to
+			Your Club" page. This means they are now members of your group.
+		</LI>
+	
+	</OL>
+
+	<LI CLASS=Normal>
+			Now you can add the new group member (or members if you added more than
+			one in step #4 above) to your team's roster-grid
+			(this is a separate step because groups can have multiple teams with
+			different group members playing on different teams).
+	</LI>
+	<OL CLASS=NumL2>
+	
+		<LI CLASS=Normal>
+			Click on the 'League History' link on the group's home page.
+		</LI>
+		
+		<LI CLASS=Normal>
+			Click on the name of the team you want to add the new member to.
+			(E.g., "2009 Fall USTA Adult Combo League").
+		</LI>
+
+		<LI CLASS=Normal>
+			At the top of the roster-grid page click on the 'EDIT' link in
+			() next to the team title.
+		</LI>
+
+		<LI CLASS=Normal>
+			On the team update page, scroll down to the 'Current Eligibility
+			For This Series' section. Here you'll see two columns, simular to the
+			Add Club Member page of step #4.a above. You'll see the new member you
+			added in step #4 above in the right-hand column. Click the 'ADD' link
+			next to their name. Once you do this they will now appear in the
+			left-hand column. The left-hand column lists all the club members who
+			are playing this particular team.
+		</LI>
+
+		<LI CLASS=Normal>
+			Click on the 'RETURN' link at the bottom of the page. This will
+			take you back to the roster-grid for this team/league. You'll see the
+			new member in the grid.
+		</LI>
+
+	</OL>
+
+</OL>
+
+
+<P>--END--</P>
+
+
+
+
+
+</P>
+
+</body>
+</html>
